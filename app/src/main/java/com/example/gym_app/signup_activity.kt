badb4cf5eaa9ebe8aaa236301aa201cf8f388815
avishkarp23 <avishkarp_t20031@students.isquareit.edu.in>
2022-11-22 18:50:43 +0530

    updated image

--- app/src/main/java/com/example/gym_app/signup_activity.kt
+++ app/src/main/java/com/example/gym_app/signup_activity.kt
@@ -3,12 +3,16 @@ package com.example.gym_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gym_app.ui.theme.Gym_appTheme
 
@@ -31,6 +35,9 @@ class signup_activity : ComponentActivity() {
 
 @Composable
 fun Signup() {
+    Box(){
+        Image(painter = painterResource(id = R.drawable.sporty), contentDescription ="logo",modifier=Modifier.fillMaxSize(), contentScale = ContentScale.Crop )
+    }
 
 }
 
